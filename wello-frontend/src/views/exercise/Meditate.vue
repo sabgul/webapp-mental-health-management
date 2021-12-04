1a8832f8d7a63c7c95b3845c5f29acef050c36eb
sabgul <sabina.gulcikova@gmail.com>
2021-12-05 00:07:56 +0100

    trends module

--- wello-frontend/src/views/exercise/Meditate.vue
+++ wello-frontend/src/views/exercise/Meditate.vue
@@ -1,6 +1,37 @@
 <template>
 <v-container>
-<v-layout row> 
+<v-layout column> 
+<div class="app">
+      <div class="vid-container">
+        <!-- <video loop >
+          <source src="./video/rain.mp4"  type="video/mp4">
+        </video> -->
+        </div>
+        <div class="time-select">
+          <v-btn data-time="10">2 Minutes</v-btn>
+          <v-btn data-time="300" class="medium-mins">5 Minutes</v-btn>
+          <v-btn data-time="600" class="long-mins">10 Minutes</v-btn>
+        </div>
+        <div class="player-container">
+            <audio class="song">
+                <source :src="require('@/assets/audio/rain.mp3')" />
+              </audio>
+              <v-img :src="require('@/assets/svg/play.svg')" class="play"></v-img>
+              <svg class="track-outline" width="453" height="453" viewBox="0 0 453 453" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="226.5" cy="226.5" r="216.5" stroke="white" stroke-width="20"/>
+                </svg>
+              <svg class="moving-outline" width="453" height="453" viewBox="0 0 453 453" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="226.5" cy="226.5" r="216.5" stroke="#018EBA" stroke-width="20"/>
+                </svg>
+              <!-- <v-img :src="require('@/assets/svg/replay.svg')" class="replay"></v-img> -->
+            
+                <h3 class="time-display">0:00</h3>      
+      </div>
+      <div class="sound-picker">
+        <v-btn :data-sound="require('@/assets/audio/rain.mp3')"><v-img :src="require('@/assets/svg/rain.svg')" alt=""></v-img></v-btn>
+        <v-btn :data-sound="require('@/assets/audio/beach.mp3')"><v-img :src="require('@/assets/svg/beach.svg')" alt="" size="50%"></v-img></v-btn>
+      </div>
+    </div>
 
     
 
@@ -15,7 +46,196 @@
     data: () => ({
     }),
 
-    methods: {
+    methods: { 
+        meditate: function meditate() {
+            const song = document.querySelector(".song");
+            const play = document.querySelector(".play");
+            const replay = document.querySelector(".replay");
+            const outline = document.querySelector(".moving-outline circle");
+            const video = document.querySelector(".vid-container video");
+            //Sounds
+            const sounds = document.querySelectorAll(".sound-picker button");
+            //Time Display
+            const timeDisplay = document.querySelector(".time-display");
+            const outlineLength = outline.getTotalLength();
+            //Duration
+            const timeSelect = document.querySelectorAll(".time-select button");
+            let fakeDuration = 600; 
+
+            outline.style.strokeDashoffset = outlineLength;
+            outline.style.strokeDasharray = outlineLength;
+            timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+            fakeDuration % 60
+            )}`; 
+
+            sounds.forEach(sound => {
+            sound.addEventListener("click", function() {
+                song.src = this.getAttribute("data-sound");
+                video.src = this.getAttribute("data-video");
+                checkPlaying(song);
+            });
+            });
+
+            play.addEventListener("click", function() {
+            checkPlaying(song);
+            });
+
+            replay.addEventListener("click", function() {
+                restartSong(song);
+                
+            }); 
+
+            const restartSong = song =>{
+                // let currentTime = song.currentTime;
+                song.currentTime = 0;
+                console.log("ciao")
+
+            }
+
+            timeSelect.forEach(option => {
+            option.addEventListener("click", function() {
+                fakeDuration = this.getAttribute("data-time");
+                timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+                fakeDuration % 60
+                )}`;
+            });
+            });
+
+            const checkPlaying = song => {
+            if (song.paused) {
+                song.play();
+                video.play();
+                play.src = "@/assets/svg/pause.svg";
+            } else {
+                song.pause();
+                video.pause();
+                play.src = "@/assets/svg/play.svg";
+            }
+            };
+
+            song.ontimeupdate = function() {
+            let currentTime = song.currentTime;
+            let elapsed = fakeDuration - currentTime;
+            let seconds = Math.floor(elapsed % 60);
+            let minutes = Math.floor(elapsed / 60);
+            timeDisplay.textContent = `${minutes}:${seconds}`;
+            let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+            outline.style.strokeDashoffset = progress;
+
+            if (currentTime >= fakeDuration) {
+                song.pause();
+                song.currentTime = 0;
+                play.src = "@/assets/svg/play.svg";
+                video.pause();
+            }
+            };
+
+        },
     },
   }
-</script>
+</script> 
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.app {
+  height: 100vh;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  /* font-family: "Montserrat", sans-serif; */
+}
+
+.sound-picker {
+  height: 80%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-direction: column;
+  flex: 1;  
+}
+
+.time-select {
+  height: 80%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-direction: column;
+  flex: 1;
+}
+.time-select button,
+.sound-picker button {
+  color: "#3DAF7B";
+  width: 30%;
+  height: 10%;
+  background: none;
+  font-size: 20px;
+  border-radius: 5px;
+  cursor: pointer;
+  border: 2px solid white;
+  transition: all 0.5s ease;
+}
+
+.sound-picker button {
+  border: none;
+  height: 120px;
+  width: 120px;
+  padding: 30px;
+  border-radius: 50%;
+}
+.sound-picker button:nth-child(1) {
+  background: #4972a1;
+}
+.sound-picker button:nth-child(2) {
+  background: #a14f49;
+}
+.sound-picker button img {
+  height: 100%;
+}
+
+.time-select button:hover {
+  background: white;
+  color: black;
+}
+
+
+.player-container {
+  position: relative;
+  height: 80%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-direction: column;
+  flex: 1;
+}
+
+.player-container svg {
+  position: absolute;
+  height: 50%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%) rotate(-90deg);
+  pointer-events: none;
+}
+.player-container svg circle {
+  transition: all 0.2s ease-in-out;
+}
+.time-display {
+  color: #000000;
+  position: absolute;
+  font-size: 50px;
+  bottom: 10%;
+}
+
+video {
+  position: fixed;
+  top: 0%;
+  left: 0%;
+  width: 100%;
+  z-index: -10;
+}
+</style>
--- wello-frontend/src/views/general/Trends.vue
+++ wello-frontend/src/views/general/Trends.vue
@@ -1,15 +1,138 @@
 <template>
-    <div>
+<div>
+<v-container justify-center>
+    <v-layout row wrap justify ="center">
+    <v-flex>
+         <v-card
+            class="ma-3 text-center"
+            color="#3DAF7B"
+            dark
+            elevation="8"
+            shaped
+            outlined
+            min-width="90%"
+        >
+    <v-card-text>
+      <v-sheet color="rgba(0, 0, 0, .12)">
+        <v-sparkline
+          :labels="labels"
+          :value="value"
+          color="rgba(255, 255, 255, .7)"
+        label-size="5"
+          padding="24"
+          stroke-linecap="round"
+          smooth
+        >
+        </v-sparkline>
+      </v-sheet>
+    </v-card-text>
 
-        <div>
-            <div>
-                <h1>
-                    TEST
-                </h1>
-            </div>
+    <v-card-text>
+      <div class="text-h6 font-weight-bold">
+        How you felt over last few days
+      </div>
+    </v-card-text>
+  </v-card>
+    </v-flex>
+</v-layout>
 
-        </div>
-    </div>
+    <v-layout row justify ="center" justify-space-between>
+        <v-flex>
+        <v-card 
+    class="ma-3"
+    color="#3DAF7B"
+    dark
+    elevation="8"
+        outlined
+    min-width="75%">
+    <v-card-title>
+        <v-icon
+        large
+        left
+        >
+            mdi-airplane-takeoff
+        </v-icon>
+        <span  class="text-h6 font-weight-bold"> What caused highs:</span>
+</v-card-title>
+    
+
+<v-card-text>
+<v-list
+color="#3DAF7B">
+    <v-list-item>
+<v-icon
+            left
+        >
+            mdi-circle-small
+        </v-icon>
+        <span  class="text-h8 font-weight-bold">had great dinner</span>
+    </v-list-item>
+    <v-list-item>
+<v-icon
+            left
+        >
+            mdi-circle-small
+        </v-icon>
+        <span  class="text-h8 font-weight-bold">went for a walk with my dog</span>
+    </v-list-item>
+
+
+</v-list>
+</v-card-text>
+    </v-card>
+        </v-flex>
+    </v-layout>
+
+
+    <v-layout row justify ="center" justify-space-between>
+        <v-flex>
+        <v-card 
+    class="ma-3"
+    color="#3DAF7B"
+    dark
+    elevation="8"
+    outlined
+    min-width="75%">
+    <v-card-title>
+        <v-icon
+            large
+            left
+            >
+                mdi-airplane-landing
+            </v-icon>
+
+            <span class="text-h6 font-weight-bold"> What caused lows: </span>
+    </v-card-title>
+
+<v-card-text>
+<v-list color="#3DAF7B">
+    <v-list-item>
+<v-icon
+            left
+        >
+            mdi-circle-small
+        </v-icon>
+        <span  class="text-h8 font-weight-bold">broke my phone</span>
+    </v-list-item>
+    <v-list-item>
+<v-icon
+            left
+        >
+            mdi-circle-small
+        </v-icon>
+        <span  class="text-h8 font-weight-bold">met my ex</span>
+    </v-list-item>
+
+
+</v-list>
+</v-card-text>
+
+
+    </v-card>
+        </v-flex>
+    </v-layout>
+</v-container>
+</div>
 </template>
 
 <script>
@@ -17,6 +140,26 @@
     name: 'Trends',
 
     data: () => ({
+        labels: [
+        '12/08/21',
+        '15/08/21',
+        '20/09/21',
+        '21/09/21',
+        '01/11/21',
+        '02/11/21',
+        '07/11/21',
+        '25/11/21',
+      ],
+      value: [
+        5,
+        4,
+        1,
+        1,
+        1,
+        3,
+        5,
+        5,
+      ],
     }),
 
     methods: {
