652dd65c2ee7487583e8ceda1525a75d5d98532a
sabinathe <41567711+sabgul@users.noreply.github.com>
2022-01-22 22:34:45 +0100

    change data source to firebase db

--- wello-app/src/main.js
+++ wello-app/src/main.js
@@ -11,9 +11,25 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import VueRouter from 'vue-router'
 import Routes from './router/router'
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 Vue.config.productionTip = false
 
 Vue.use(VueRouter);
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDoJm1ldoyo8lrR19DRCiUAFcaStR_YZOw",
+  authDomain: "wello-83193.firebaseapp.com",
+  projectId: "wello-83193",
+  storageBucket: "wello-83193.appspot.com",
+  messagingSenderId: "813665035793",
+  appId: "1:813665035793:web:9158b4b397b8f3136573b0",
+  measurementId: "G-5M447F0DQ1"
+});
+
+export const db = firebase.firestore();
 
 const router = new VueRouter({
   mode: 'history',
